d58906d6f44e8ad34728266c2b839006c6b6b884
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-12-13 00:17:54 +0800

    updated main and added config toml

--- main.py
+++ main.py
@@ -38,47 +38,7 @@ rain(
 
 if st.button('Start Game'):
     switch_page("game")
-# Links to other Pages
-
-# st.header('Waffle Score: 0')
-# st.subheader('Total: 0')
-#
-# st.markdown("<br>", unsafe_allow_html=True)
-#
-#
-#
-# all_images = ["images/waffle.png", "images/hacker.png"]
-# display_img = random.choice(all_images)
-# print(display_img)
-# # quiz_image = Image.open('images/questionmark.png')
-# quiz_image = Image.open(display_img)
-# st.image(quiz_image, width=900)
-#
-# col1, col2 = st.columns(2)
-#
-# with col1:
-#     if st.button('Waffle'):
-#         st.write('Waffle')
-#     else:
-#         st.write('Hacker')
-#
-#
-# with col2:
-#     if st.button('Hacker'):
-#         st.write('Hacker')
-#     else:
-#         st.write('Hacker')
 
 
-# with col3:
-#     image_login_path = "images/login_btn.png"
-#     login_image_base64 = convert_image(image_login_path)
-#
-#     html = f"<a href='#'><img src='data:image/png;base64,{login_image_base64}'  style='border-radius: 30px;'></a>"
-#     st.markdown(html, unsafe_allow_html=True)
-#
-#     st.write("Admin Login")
-
-
-st.markdown("<br><br>", unsafe_allow_html=True)
+# st.markdown("<br><br>", unsafe_allow_html=True)
 
