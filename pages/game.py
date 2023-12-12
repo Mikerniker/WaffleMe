3922f67e07c9dd26c57f52baeeab47b4c0dc8ea9
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-12-13 00:21:02 +0800

    added css and added gamt_btn to session state

--- pages/game.py
+++ pages/game.py
@@ -9,9 +9,30 @@ st.set_page_config(
     layout="wide",
 )
 
-# Create a session state variable
+# ADD CSS
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+def remote_css(url):
+    st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)
+
+local_css("style.css")
+remote_css('https://fonts.googleapis.com/css2?family=Afacad:wght@700&display=swap')
+
+
+# Create session state variables
 if 'score' not in st.session_state:
     st.session_state.score = 0
+
+if 'game_state' not in st.session_state:
+    st.session_state.game_state = 'start'  # Initial game state
+
+if 'game_btn' not in st.session_state:
+    st.session_state.game_btn = False
+
+
 
 
 st.header('Is it a waffle or a hacker?')
@@ -83,27 +104,41 @@ with col2:
 
 if check_score(user_guess, display[0]):
     st.session_state.score += 1
+    message = "You got it!"
+    st.write()
+else:
+    message = "Try again!"
 
         # if check_score(display[0], player_choices):
         #     score += 1
 
 countdown_placeholder = st.empty()
 
-for i in range(10, -1, -1):
-    time_str = countdown(i)
-    countdown_placeholder.write(f"Time remaining: {time_str}")
-    time.sleep(1)
-
-game_on = True
-
-if game_on:
-    quiz_image = Image.open(display[1])
-    st.image(quiz_image, width=600)
-else:
+countdown_on = True
+if countdown_on:
     quiz_image = Image.open('images/questionmark.png')
     st.image(quiz_image, width=600)
+    for i in range(10, -1, -1):
+        time_str = countdown(i)
+        countdown_placeholder.write(f"Time remaining: {time_str}")
+        time.sleep(1)
+    countdown_on = False
+
+st.write(f"{message}")
+st.write("Guess again to continue playing")
+
+# game_on = True
+
+# if game_on:
+quiz_image = Image.open(display[1])
+st.image(quiz_image, width=600)
+# else:
+#     quiz_image = Image.open('images/questionmark.png')
+#     st.image(quiz_image, width=600)
 st.write(f"Score is: {st.session_state.score} ")
 # player_choices = []
+
+
 
 # ORIGINAL
 
