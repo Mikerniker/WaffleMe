7f9f8af108271d2453e5301635f445dcd72efee9
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-12-14 01:35:54 +0800

    created functions.py and modified main.py

---
+++ functions.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import random
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+def remote_css(url):
+    st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)
+
+
+def start_game_btn():
+    st.session_state.game_btn = True
+
+
+def check_score(guess, computer):
+    if guess == computer:
+        return True
+
+
+def show_image():
+    all_images = [("waffle", "images/waffle.png"), ("hacker", "images/hacker.png")]
+    display_img = random.choice(all_images)
+    image_path = display_img[1]
+    image_choice = display_img[0]
+    return image_choice, image_path
+
+
+def countdown(time_sec, countdown_placeholder):
+    if time_sec >= 0:
+        mins, secs = divmod(time_sec, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        countdown_placeholder.write(f"#### Time remaining: {timeformat}")
+        # time.sleep(1)
+
+
+def hacker_image():
+    hacker_image_paths = ["images/hackerwaffle.png", "images/hackerwaffle3.png"]
+    display_img = random.choice(hacker_image_paths)
+    return display_img
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PIL import Image
 import random
 from streamlit_extras.switch_page_button import switch_page
 from streamlit_extras.let_it_rain import rain
-
+from functions import local_css, remote_css
 
 
 st.set_page_config(
@@ -15,29 +15,30 @@ st.set_page_config(
 
 
 # Add CSS
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
-def remote_css(url):
-    st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)
-
 local_css("style.css")
 remote_css('https://fonts.googleapis.com/css2?family=Afacad:wght@700&display=swap')
+
 # Banner Section
 banner_image = Image.open('images/waffle2.png')
 
 st.image(banner_image, width=900)
 
-rain(
-    emoji="🧇",
-    font_size=54,
-    falling_speed=5,
-    animation_length="infinite",
-)
+# rain(
+#     emoji="🧇",
+#     font_size=54,
+#     falling_speed=5,
+#     animation_length="infinite",
+# )
+st.write('# Is it a waffle or a hacker?')
+st.write('## Make your best guess at whats behind the question'
+         ' mark. Choose a button before the time runs out '
+         'or a hacker will steal your waffles! ')
 
 if st.button('Start Game'):
-    switch_page("game")
+    st.session_state.game_state = 'playing'
+    switch_page("test")
+# if st.button('Start Game'):
+#     switch_page("start_game")
 
 
 # st.markdown("<br><br>", unsafe_allow_html=True)
