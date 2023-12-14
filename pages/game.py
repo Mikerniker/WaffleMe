dd77fca830da8c6699be472f942b2dd2f639b75b
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-12-15 04:15:40 +0800

    deleted functions that were moved to functions.py

--- pages/game.py
+++ pages/game.py
@@ -38,36 +38,6 @@ hacker_image = Image.open(hacker_image())
 game_placeholder = None
 
 
-def start_game_btn():
-    st.session_state.game_btn = True
-
-
-def check_score(guess, computer):
-    if guess == computer:
-        return True
-
-
-def show_image():
-    all_images = [("waffle", "images/waffle.png"), ("hacker", "images/hacker.png")]
-    display_img = random.choice(all_images)
-    image_path = display_img[1]
-    image_choice = display_img[0]
-    return image_choice, image_path
-
-
-def countdown(time_sec, countdown_placeholder):
-    if time_sec >= 0:
-        mins, secs = divmod(time_sec, 60)
-        timeformat = '{:02d}:{:02d}'.format(mins, secs)
-        countdown_placeholder.write(f"#### Time remaining: {timeformat}")
-        # time.sleep(1)
-
-
-def hacker_image():
-    hacker_image_paths = ["images/hackerwaffle.png", "images/hackerwaffle2.png"]
-    display_img = random.choice(hacker_image_paths)
-    return display_img
-
 display = show_image()
 
 with col1:
