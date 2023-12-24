2f56838c18509484127d2352dc7e3efcbe3e6bf9
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-12-25 02:00:33 +0800

    modified if else by adding time sleep in game_state

--- pages/game.py
+++ pages/game.py
@@ -89,11 +89,11 @@ while st.session_state.game_state == 'playing':
                 st.session_state.game_state = 'end'
                 play_game = False
 
-
             else:
                 if check_score(user_guess, display[0]):
                     st.session_state.score += 1
                     message = "You're right, you get waffle points!!"
+                    time.sleep(4)
                 else:
                     message = "Wrong choice...you've been shenanihacked and " \
                               "your waffles have been stolen!"
